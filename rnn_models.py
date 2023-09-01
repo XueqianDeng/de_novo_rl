dc3d6b1be86c82c2106d46fd2a7056015c0cc29a
XueqianDeng <denghokin@gmail.com>
2023-09-01 19:01:38 -0400

    training code

--- rnn_models.py
+++ rnn_models.py
@@ -17,3 +17,109 @@ def loss_mse(output, target, mask):
     # Account for different number of masked values per trial
     loss_by_trial = loss_tensor.sum(dim=-1) / mask[:, :, 0].sum(dim=-1)
     return loss_by_trial.mean()
+
+def train(net, _input, _target, _mask, n_epochs, lr=1e-2, batch_size=32, plot_learning_curve=False, plot_gradient=False,
+          mask_gradients=False, clip_gradient=None, early_stop=None, keep_best=False, cuda=False, resample=False):
+    """
+    Train a network
+    :param net: nn.Module
+    :param _input: torch tensor of shape (num_trials, num_timesteps, input_dim)
+    :param _target: torch tensor of shape (num_trials, num_timesteps, output_dim)
+    :param _mask: torch tensor of shape (num_trials, num_timesteps, 1)
+    :param n_epochs: int
+    :param lr: float, learning rate
+    :param batch_size: int
+    :param plot_learning_curve: bool
+    :param plot_gradient: bool
+    :param mask_gradients: bool, set to True if training the SupportLowRankRNN_withMask for reduced models
+    :param clip_gradient: None or float, if not None the value at which gradient norm is clipped
+    :param early_stop: None or float, set to target loss value after which to immediately stop if attained
+    :param keep_best: bool, if True, model with lower loss from training process will be kept (for this option, the
+        network has to implement a method clone())
+    :param resample: for SupportLowRankRNNs, set True
+    :return: nothing
+    """
+    print("Training...")
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    num_examples = _input.shape[0]
+    all_losses = []
+    if plot_gradient:
+        gradient_norms = []
+
+    # CUDA management
+    if cuda:
+        if not torch.cuda.is_available():
+            print("Warning: CUDA not available on this machine, switching to CPU")
+            device = torch.device('cpu')
+        else:
+            device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    net.to(device=device)
+    input = _input.to(device=device)
+    target = _target.to(device=device)
+    mask = _mask.to(device=device)
+
+    # Initialize setup to keep best network
+    with torch.no_grad():
+        initial_loss = loss_mse(net(input), target, mask)
+        print("initial loss: %.3f" % (initial_loss.item()))
+        if keep_best:
+            best = net.clone()
+            best_loss = initial_loss.item()
+
+    # Training loop
+    for epoch in range(n_epochs):
+        begin = time.time()
+        losses = []  # losses over the whole epoch
+        for i in range(num_examples // batch_size):
+            optimizer.zero_grad()
+            random_batch_idx = random.sample(range(num_examples), batch_size)
+            batch = input[random_batch_idx]
+            output = net(batch)
+            loss = loss_mse(output, target[random_batch_idx], mask[random_batch_idx])
+            losses.append(loss.item())
+            all_losses.append(loss.item())
+            loss.backward()
+            if mask_gradients:
+                net.m.grad = net.m.grad * net.m_mask
+                net.n.grad = net.n.grad * net.n_mask
+                net.wi.grad = net.wi.grad * net.wi_mask
+                net.wo.grad = net.wo.grad * net.wo_mask
+                net.unitn.grad = net.unitn.grad * net.unitn_mask
+                net.unitm.grad = net.unitm.grad * net.unitm_mask
+                net.unitwi.grad = net.unitwi.grad * net.unitwi_mask
+            if clip_gradient is not None:
+                torch.nn.utils.clip_grad_norm_(net.parameters(), clip_gradient)
+            if plot_gradient:
+                tot = 0
+                for param in [p for p in net.parameters() if p.requires_grad]:
+                    tot += (param.grad ** 2).sum()
+                gradient_norms.append(sqrt(tot))
+            optimizer.step()
+            # These 2 lines important to prevent memory leaks
+            loss.detach_()
+            output.detach_()
+            if resample:
+                net.resample_basis()
+        if keep_best and np.mean(losses) < best_loss:
+            best = net.clone()
+            best_loss = np.mean(losses)
+            print("epoch %d:  loss=%.3f  (took %.2f s) *" % (epoch, np.mean(losses), time.time() - begin))
+        else:
+            print("epoch %d:  loss=%.3f  (took %.2f s)" % (epoch, np.mean(losses), time.time() - begin))
+        if early_stop is not None and np.mean(losses) < early_stop:
+            break
+
+    if plot_learning_curve:
+        plt.plot(all_losses)
+        plt.title("Learning curve")
+        plt.show()
+
+    if plot_gradient:
+        plt.plot(gradient_norms)
+        plt.title("Gradient norm")
+        plt.show()
+
+    if keep_best:
+        net.load_state_dict(best.state_dict())
