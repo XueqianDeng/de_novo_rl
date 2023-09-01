979be8cdbb5472965830ed8d0b3748ab907e4672
XueqianDeng <denghokin@gmail.com>
2023-09-01 18:59:56 -0400

    add mse

--- rnn_models.py
+++ rnn_models.py
@@ -4,3 +4,16 @@ from math import sqrt, floor
 import random
 import time
 
+def loss_mse(output, target, mask):
+    """
+    Mean squared error loss
+    :param output: torch tensor of shape (num_trials, num_timesteps, output_dim)
+    :param target: torch tensor of shape (num_trials, num_timesteps, output_dim)
+    :param mask: torch tensor of shape (num_trials, num_timesteps, 1)
+    :return: float
+    """
+    # Compute loss for each (trial, timestep) (average accross output dimensions)
+    loss_tensor = (mask * (target - output)).pow(2).mean(dim=-1)
+    # Account for different number of masked values per trial
+    loss_by_trial = loss_tensor.sum(dim=-1) / mask[:, :, 0].sum(dim=-1)
+    return loss_by_trial.mean()
